9edc9b0318aebaad24e77300c4fcab3bccb78d8b
Bindupriyadarshini.P <bindupriyadarshini.pulukuri@ggktech.com>
2017-09-27 22:47:40 +0530

    .NetStandard and .NetFramework encryption and decryption

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDataProtection();
+            var services = serviceCollection.BuildServiceProvider();
+
+            // create an instance of MyClass using the service provider
+            var instance = ActivatorUtilities.CreateInstance<ClassLibrary1.Class1>(services);
+            
+                      
+            var x=instance.GetStringValue();
+
+            Console.WriteLine("protected Payload:"+x.protectedPayload);
+            Console.WriteLine("uprotected Payload:" + x.unprotectedPayload);
+            Console.ReadLine();
+            ClassLibrary2.Class1 cls = new ClassLibrary2.Class1();
+
+            var x1= cls.GetStrValue();
+
+            Console.WriteLine("protected Payload:" + x1.protectedPayload);
+            Console.WriteLine("uprotected Payload:" + x1.unprotectedPayload);
+            Console.ReadLine();
+
+
+
+
+        }
+        public class EncrypDecryptModel
+        {
+
+            public string unprotectedPayload { get; set; }
+            public string protectedPayload { get; set; }
+
+        }
+    }
+}
---
+++ SampleWebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibrary;
+namespace SampleWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+            ClassLibrary.Class1 cls = new ClassLibrary.Class1();
+           
+        }
+    }
+}
